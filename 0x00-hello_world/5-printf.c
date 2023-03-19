cb0df7ab2bc8aef53e04d609b5530581def6bd07
allan-roxx <allanroxx78@gmail.com>
2023-03-19 01:29:49 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ * main - Aprogram that prints to screen using printf
+ * Return: 0(success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+}
