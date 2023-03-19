dbfa77add676b0d519d97574e9ae4759cd4faf39
allan-roxx <allanroxx78@gmail.com>
2023-03-19 02:08:40 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
- * main - Write a C program that prints the size of various types on the computer.
- * Return: 0(success)
+ * main - Write a C program that prints the size of various
+ * Return: 0(success
  */
 int main(void)
 {
